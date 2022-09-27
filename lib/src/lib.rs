6e4d5ae84829fc8349c7796c85702a52bf423a00
Rajeev Karamcheti <rkaramc@gmail.com>
2022-09-27 18:15:42 +0530

    tests unit and integration

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -7,3 +7,11 @@ pub struct State {
 pub fn step(state: &mut State) {
     state.counter += 1;
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(4, 2 + 2);
+    }
+}
